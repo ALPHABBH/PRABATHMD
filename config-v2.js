f30f130ffc4da7798bfd75f4b0a1a7630d68f37f
ALPHABBH <eshankalpana72@gmail.com>
2024-11-22 07:45:19 +0530

    Update config-v2.js

--- config-v2.js
+++ config-v2.js
@@ -1,6 +1,6 @@
-const GITHUB_AUTH_TOKEN = "null"
-const BOT_NUMBER = "null"
-const SESSION_ID = ""
+const GITHUB_AUTH_TOKEN = "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"
+const BOT_NUMBER = "94703266143"
+const SESSION_ID = "PRABATH-MD~9z1yyKCI#QZm1dYB7xuXugIcHSBBe7it2px8VbmzFTJ_lbIrBuh8"
 
 
 module.exports = {
